d761e44cda71b29c4dd30e4883f3ef84c0b5a898
Danniel Navas <dannielnavas@gmail.com>
2026-06-23 07:47:15 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <div class="w-full h-full  min-h-screen flex items-center justify-center dark:bg-gray-600 dark:text-white">
+      <button class="text-2xl fixed top-0 right-0">🌚</button>
+
+      <div>
+        <h1 class="text-2xl text-center">Solid Todo App</h1>
+        <input class="border dark:text-black" type="text" />
+        <button class="px-2 border">Add</button>
+        <ul>
+          <li>
+            <input type="checkbox" checked />
+            <span>
+              <s>abrazaar pinguino</s>
+            </span>
+            <button>❌</button>
+          </li>
+          <li>
+            <input type="checkbox" checked />
+            <span>
+              <s>abrazaar pinguino</s>
+            </span>
+            <button>❌</button>
+          </li>
+        </ul>
+        <p class="text-sm mt-4">Completed count: {0}</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
