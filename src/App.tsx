526a9000071989ad9dcdf7293e48c1fc2eacdfe2
Danniel Navas <dannielnavas@gmail.com>
2026-06-24 07:49:10 -0500

    agregar funcionalidad para mostrar, alternar y eliminar tareas en la lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createEffect, createSignal, Show, type Component } from "solid-js";
+import { createEffect, createSignal, For, Show, type Component } from "solid-js";
 
 const App: Component = () => {
   const [darkMode, setDarkMode] = createSignal(false);
@@ -29,11 +29,51 @@ const App: Component = () => {
         <input class="border dark:text-black" type="text" />
         <button class="px-2 border">Add</button>
         <ul>
-          {todos().map((todo) => (
+          <For each={todos()}>
+            {(
+              todo,
+              index, // el index es un signal que nos permite saber el indice del elemento en el array, es decir, nos permite saber en que posicion del array se encuentra el elemento que estamos iterando
+            ) => (
+              <li>
+                <input
+                  type="checkbox"
+                  checked={(console.log("common operator"), todo.completed)}
+                  onChange={() =>
+                    setTodos((prev) =>
+                      prev.map((t) =>
+                        t.id === todo.id ? { ...t, completed: !t.completed } : t,
+                      ),
+                    )
+                  }
+                />
+                <span
+                  onclick={() =>
+                    setTodos((prev) =>
+                      prev.map((t) =>
+                        t.id === todo.id ? { ...t, completed: !t.completed } : t,
+                      ),
+                    )
+                  }
+                >
+                  <Show when={todo.completed} fallback={<span>{todo.text}</span>}>
+                    <s>{todo.text}</s>
+                  </Show>
+                </span>
+                <button
+                  onclick={() =>
+                    setTodos((prev) => prev.filter((t) => t.id !== todo.id))
+                  }
+                >
+                  ❌
+                </button>
+              </li>
+            )}
+          </For>
+          {/* {todos().map((todo) => (
             <li>
               <input
                 type="checkbox"
-                checked={todo.completed}
+              checked={(console.log('common operator'), todo.completed)} // ejecuta lo que esta en el console.log y luego retorna el valor de todo.completed de  esta forma se ejecuta tres veces el console.log por cada todo que se renderiza
                 onChange={() =>
                   setTodos((prev) =>
                     prev.map((t) =>
@@ -63,7 +103,7 @@ const App: Component = () => {
                 ❌
               </button>
             </li>
-          ))}
+          ))} */}
           {/* <li>
             <input type="checkbox" checked />
 
