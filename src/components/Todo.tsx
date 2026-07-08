f4d93207c9ac5b833d7036ea95f1990832700073
Danniel Navas <dannielnavas@gmail.com>
2026-07-08 07:36:40 -0500

    refactor: eliminar funciones innecesarias y simplificar el componente Todo

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,19 +1,14 @@
 import { JSX, Show } from "solid-js";
-import { produce, SetStoreFunction } from "solid-js/store";
 
 export function Todo(props: {
   todo: { text: string; completed: boolean };
   index: () => number;
-  removeTodos: (index: number) => void;
-  setTodos: SetStoreFunction<
-    {
-      text: string;
-      completed: boolean;
-    }[]
-  >;
+  onRemove: () => void;
   children: JSX.Element;
+  onInputChange: () => void;
+  onTextChange: (newText: string) => void;
 }) {
-  const { todo, index, removeTodos, setTodos } = props;
+  const { todo, index } = props;
   return (
     <>
       <input
@@ -21,11 +16,12 @@ export function Todo(props: {
         checked={(console.log("common operator"), todo.completed)}
         onChange={() => {
           // setTodos(index(), "completed", !todo.completed)
-          setTodos(
-            produce((todos) => {
-              todos[index()].completed = !todos[index()].completed;
-            }),
-          );
+          // setTodos(
+          //   produce((todos) => {
+          //     todos[index()].completed = !todos[index()].completed;
+          //   }),
+          // );
+          props.onInputChange();
         }}
       />
       <span
@@ -39,18 +35,19 @@ export function Todo(props: {
           target.removeAttribute("contenteditable");
           // todo.setText(target.innerText);
           // setTodos(index(), "text", target.innerText);
-          setTodos(
-            produce((todos) => {
-              todos[index()].text = target.innerText;
-            }),
-          );
+          // setTodos(
+          //   produce((todos) => {
+          //     todos[index()].text = target.innerText;
+          //   }),
+          // );
+          props.onTextChange(target.innerText);
         }}
       >
         <Show when={todo.completed} fallback={<span>{props.children}</span>}>
           <s>{props.children}</s>
         </Show>
       </span>
-      <button onclick={() => removeTodos(index())}>❌</button>
+      <button onclick={() => props.onRemove()}>❌</button>
     </>
   );
 }
